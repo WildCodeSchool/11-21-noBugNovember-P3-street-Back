a5e5b913f7cc521d3f2e83b3f64ce1a8311b7f7a
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2022-02-16 16:40:06 +0100

    Update Routes All

--- src/routes/models/functions.js
+++ src/routes/models/functions.js
@@ -3,32 +3,45 @@ const Joi = require('joi')
 //const argon2 = require('argon2')
 const db = connection.promise()
 
+//Trouver un utilisateur en particulier
 const findUser = id => {
   return db
     .query(
-      'SELECT u.firstname, u.lastname, u.avatar, u.email, u.emailVisibility, u.phone, u.phoneVisibility, u.country, u.city, u.birthday, u.twitter, u.instagram, u.youtube, u.spotify, sd.art_name, d.name FROM users AS u INNER JOIN domain AS d INNER JOIN users_has_domain AS uhd INNER JOIN sub_domain AS sd INNER JOIN sub_domain_has_users AS sdhu ON u.id=uhd.users_id AND uhd.domain_id = d.id AND u.id=sdhu.users_id AND sdhu.sub_domain_id=sd.id WHERE u.id=1 AND u.blocked=0',
+      'SELECT u.firstname, u.lastname, u.avatar, u.email, u.emailVisibility, u.phone, u.phoneVisibility, u.country, u.city, u.birthday, u.twitter, u.instagram, u.youtube, u.spotify, sd.art_name, d.domain FROM users AS u INNER JOIN domain AS d INNER JOIN users_has_domain AS uhd INNER JOIN sub_domain AS sd INNER JOIN sub_domain_has_users AS sdhu ON u.id=uhd.users_id AND uhd.domain_id = d.id AND u.id=sdhu.users_id AND sdhu.sub_domain_id=sd.id WHERE u.id=? AND u.blocked=0',
       [id]
     )
     .then(([results]) => results[0])
 }
 
+//Choisir un projet en particulier
 const findProject = id => {
   return db
     .query(
-      'SELECT p.name, p.logo, p.estimated_start_date, p.estimated_end_date, p.description, p.team_completed, p.status, p.localisation, u.firstname, u.lastname, d.name FROM project AS p INNER JOIN users AS u INNER JOIN domain AS d ON d.id=p.domain_id AND u.id=p.users_id WHERE p.id=?',
+      'SELECT p.name, p.logo, p.estimated_start_date, p.estimated_end_date, p.description, p.team_completed, p.status, p.localisation, u.firstname, u.lastname, d.domain FROM project AS p INNER JOIN users AS u INNER JOIN domain AS d ON d.id=p.domain_id AND u.id=p.users_id WHERE p.id=?',
       [id]
     )
     .then(([results]) => results[0])
 }
 
-const projects = () => {
+//Liste des projets
+const projects = id => {
   return db
     .query(
-      'SELECT p.name, p.logo, p.estimated_start_date, p.estimated_end_date, p.description, p.team_completed, p.status, p.localisation, u.firstname, u.lastname, d.domain FROM project AS p INNER JOIN users AS u INNER JOIN domain AS d ON d.id=p.domain_id AND u.id=p.users_id'
+      'SELECT p.name, p.logo, p.estimated_start_date, p.estimated_end_date, p.description, p.team_completed, p.status, p.localisation, d.domain FROM project AS p INNER JOIN domain AS d ON d.id=p.domain_id ORDER BY p.id DESC'
     )
     .then(([results]) => results)
 }
 
+//Obtenir la liste des participants à un projet (hors créateur)
+const projectshasuser = () => {
+  return db
+    .query(
+      'SELECT p.name, p.logo, p.status, d.domain FROM project AS p INNER JOIN users AS u INNER JOIN domain AS d INNER JOIN project_has_users AS phu ON d.id=p.domain_id AND phu.users_id=u.id AND p.id=phu.project_id WHERE u.id=?'
+    )
+    .then(([results]) => results)
+}
+
+//Obtenir la liste des annonces des users
 const findAnnoncesUsers = () => {
   return db
     .query(
@@ -37,6 +50,7 @@ const findAnnoncesUsers = () => {
     .then(([results]) => results)
 }
 
+//Obtenir l'annonce d'un utilisateur
 const findAnnonceUser = id => {
   return db
     .query(
@@ -46,6 +60,7 @@ const findAnnonceUser = id => {
     .then(([results]) => results[0])
 }
 
+//Valider les données de la création d'un compte
 const validate = (data, forCreation = true) => {
   const presence = forCreation ? 'required' : 'optional'
   return Joi.object({
@@ -73,11 +88,16 @@ const validate = (data, forCreation = true) => {
   }).validate(data, { abortEarly: false }).error
 }
 
+/*
+SELECT p.name, p.logo, p.estimated_start_date, p.estimated_end_date, p.description, p.team_completed, p.status, p.localisation, u.firstname, u.lastname, d.domain FROM project AS p INNER JOIN users AS u INNER JOIN domain AS d INNER JOIN project_has_users AS phu ON d.id=p.domain_id AND u.id=p.users_id AND p.id=phu.project_id AND phu.users_id=u.id WHERE u.id=?;
+*/
+
 module.exports = {
   findUser,
   findProject,
   findAnnoncesUsers,
   findAnnonceUser,
-  validate,
-  projects
+  projects,
+  projectshasuser,
+  validate
 }
