1c429e289e265afcb10f3ac140671e654c79233f
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2022-02-16 15:35:33 +0100

    Update

--- src/routes/all.js
+++ src/routes/all.js
@@ -4,23 +4,37 @@ const functions = require('./models/functions')
 const Router = express.Router()
 
 //Obtenir les détails d'un projet
-Router.get('/project_details/:id', (req, res) => {
-  console.log(req.params)
-  const sql =
-    'SELECT p.name, p.logo, p.estimated_start_date, p.estimated_end_date, p.description, p.team_completed, p.status, p.localisation, u.firstname, u.lastname, d.name FROM project AS p INNER JOIN users AS u INNER JOIN domain AS d ON d.id=p.domain_id AND u.id=p.users_id WHERE p.id=?;'
-  const value = [req.params.id]
-
-  connection.query(sql, value, (err, result) => {
-    if (err) throw err
-    return res.status(200).json(result)
-  })
-  console.log('GET on All/project_details')
+Router.get('/project_details', (req, res) => {
+  functions
+    .findProject(req.body.id)
+    .then(user => {
+      if (user) res.json(user)
+      else res.status(404).send('Project not found')
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).send('Error retrieving Project from database')
+    })
 })
 
-Router.get('/users', (req, res) => {
-  console.log(req.body)
+//Obtenir la liste des projets
+Router.get('/projects', (req, res) => {
   functions
-    .findUsers(req.body.id)
+    .projects()
+    .then(user => {
+      if (user) res.json(user)
+      else res.status(404).send('Project not found')
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).send('Error retrieving Project from database')
+    })
+})
+
+//Obtenir les infos d'un utilisateur
+Router.get('/user', (req, res) => {
+  functions
+    .findUser(req.body.id)
     .then(user => {
       if (user) res.json(user)
       else res.status(404).send('User not found')
@@ -28,6 +42,34 @@ Router.get('/users', (req, res) => {
     .catch(err => {
       console.error(err)
       res.status(500).send('Error retrieving user from database')
+    })
+})
+
+//Avoir toutes les annonces de tous les utilisateurs
+Router.get('/annonces_all_users', (req, res) => {
+  functions
+    .findAnnoncesUsers()
+    .then(user => {
+      if (user) res.json(user)
+      else res.status(404).send('Annonce not found')
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).send('Error retrieving annonce from database')
+    })
+})
+
+//Annonce de l'annonce d'un utilisateur
+Router.get('/annonce_user', (req, res) => {
+  functions
+    .findAnnonceUser(req.body.id)
+    .then(user => {
+      if (user) res.json(user)
+      else res.status(404).send('Annonce not found')
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).send('Error retrieving annonce from database')
     })
 })
 
--- src/routes/models/functions.js
+++ src/routes/models/functions.js
@@ -3,23 +3,81 @@ const Joi = require('joi')
 //const argon2 = require('argon2')
 const db = connection.promise()
 
-const findUsers = id => {
+const findUser = id => {
   return db
-    .query('SELECT * FROM users WHERE id = ?', [id])
+    .query(
+      'SELECT u.firstname, u.lastname, u.avatar, u.email, u.emailVisibility, u.phone, u.phoneVisibility, u.country, u.city, u.birthday, u.twitter, u.instagram, u.youtube, u.spotify, sd.art_name, d.name FROM users AS u INNER JOIN domain AS d INNER JOIN users_has_domain AS uhd INNER JOIN sub_domain AS sd INNER JOIN sub_domain_has_users AS sdhu ON u.id=uhd.users_id AND uhd.domain_id = d.id AND u.id=sdhu.users_id AND sdhu.sub_domain_id=sd.id WHERE u.id=1 AND u.blocked=0',
+      [id]
+    )
     .then(([results]) => results[0])
 }
-/*
+
+const findProject = id => {
+  return db
+    .query(
+      'SELECT p.name, p.logo, p.estimated_start_date, p.estimated_end_date, p.description, p.team_completed, p.status, p.localisation, u.firstname, u.lastname, d.name FROM project AS p INNER JOIN users AS u INNER JOIN domain AS d ON d.id=p.domain_id AND u.id=p.users_id WHERE p.id=?',
+      [id]
+    )
+    .then(([results]) => results[0])
+}
+
+const projects = () => {
+  return db
+    .query(
+      'SELECT p.name, p.logo, p.estimated_start_date, p.estimated_end_date, p.description, p.team_completed, p.status, p.localisation, u.firstname, u.lastname, d.domain FROM project AS p INNER JOIN users AS u INNER JOIN domain AS d ON d.id=p.domain_id AND u.id=p.users_id'
+    )
+    .then(([results]) => results)
+}
+
+const findAnnoncesUsers = () => {
+  return db
+    .query(
+      'SELECT ad.description, ad.date, u.avatar, u.firstname, u.lastname FROM annonces_dispo AS ad INNER JOIN users AS u ON u.id = ad.users_id'
+    )
+    .then(([results]) => results)
+}
+
+const findAnnonceUser = id => {
+  return db
+    .query(
+      'SELECT ad.description, ad.date, u.avatar, u.firstname, u.lastname FROM annonces_dispo AS ad INNER JOIN users AS u ON u.id = ad.users_id WHERE u.id=?',
+      [id]
+    )
+    .then(([results]) => results[0])
+}
+
 const validate = (data, forCreation = true) => {
-  const presence = forCreation ? 'required' : 'optional';
+  const presence = forCreation ? 'required' : 'optional'
   return Joi.object({
-    title: Joi.string().max(255).presence(presence),
-    director: Joi.string().max(255).presence(presence),
-    year: Joi.number().integer().min(1888).presence(presence),
-    color: Joi.boolean().presence(presence),
-    duration: Joi.number().integer().min(1).presence(presence),
-  }).validate(data, { abortEarly: false }).error;
-};
-*/
+    admin: Joi.boolean().presence(presence).required(),
+    blocked: Joi.boolean().presence(presence).required(),
+    firstname: Joi.string().max(45).presence(presence).required(),
+    lastname: Joi.string().max(45).presence(presence).required(),
+    nickname: Joi.string().max(45),
+    password: Joi.string().max(255).presence(presence).required(),
+    avatar: Joi.string().max(255),
+    email: Joi.string.max(255).presence(presence).required(),
+    phone: Joi.number().presence(),
+    city: Joi.string().max(255),
+    country: Joi.string().max(255),
+    birthday: Joi.date().presence(presence).required(),
+    twitter: Joi.string().max(255),
+    instagram: Joi.string().max(255),
+    youtube: Joi.string().max(255),
+    spotify: Joi.string().max(255),
+    description: Joi.string(),
+    forget_password: Joi.string().max(255).required(),
+    available: Joi.boolean().required(),
+    phoneVisibilty: Joi.boolean().required(),
+    emailVisibilty: Joi.boolean().required()
+  }).validate(data, { abortEarly: false }).error
+}
+
 module.exports = {
-  findUsers
+  findUser,
+  findProject,
+  findAnnoncesUsers,
+  findAnnonceUser,
+  validate,
+  projects
 }
